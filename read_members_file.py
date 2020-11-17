d7abe6faa78f5107e4b016d8632ae1a21cdb1203
Mathias <mathias.schoenhofer@gmail.com>
2020-11-17 15:30:34 +0100

    implemented call_stats function

--- read_members_file.py
+++ read_members_file.py
@@ -65,8 +65,16 @@ def calc_stats(taxids, all_taxids):
     """
     # Basic argument checking
     assert len(set(all_taxids)) == len(all_taxids), "Taxids occur more than once?"
+
     # Your code here
-    return (False, False, False)
+    # Compares the number of different taxids to the total number of taxids --> occurance 
+    occ = len(set(taxids))*100/len(all_taxids)
+    # Compares the number of unique taxids to the number off different taxids --> uniqueness 
+    uniq = len(uniques(taxids))*100/len(set(taxids))
+    # Multiplies the percentage of unique taxids in "taxids" by the fraction of all taxids occuring in "taxids" --> occurance single copy 
+    occ_sc = occ*uniq/100    
+    
+    return (occ, uniq, occ_sc)
 
 
 def parse_members_file(fin):
